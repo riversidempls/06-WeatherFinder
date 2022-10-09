2a838fd72feb9e9e67c55df1755049d9b703c463
Conor Donnelly <Conor.Donnelly@state.mn.us>
2022-10-09 13:49:22 -0500

    struggling with local storage

--- assets/script.js
+++ assets/script.js
@@ -1,38 +1,43 @@
 
 // This is my API key for OpenWeatherMap
-var appid = 'bdc63249ca9c768d065db41d5ee05a7d'
+var appid = 'bdc63249ca9c768d065db41d5ee05a7d';
 
-var baseURL = 'https://api.openweathermap.org'
+var baseURL = 'https://api.openweathermap.org';
 
-var geoURLSuffix = {baseURL}+'/geo/1.0/direct?q='+{cityEntered}+'&limit=5&appid='+{appid}
+var geoURLSuffix = {baseURL}+'/geo/1.0/direct?q='+{cityEntered}+'&limit=5&appid='+{appid};
 
-var cityEntered
+var cityEntered = 1;
 
-var citySearchList
+var citySearchList = [];
 
-var citySelectList
+var citySelectList = 1;
 
-var day2
+var day2 = 1;
 
-var day3
+var day3 = 1;
 
-function addCity() {
-    // Parse any JSON previously stored in allEntries
-    var existingEntries = JSON.parse(localStorage.getItem("allEntries"));
-    if(existingEntries == null) existingEntries = [];
-    var cityText = document.getElementById("cityText").value;
-    var entry = {
-        "text": cityText
-    };
-    localStorage.setItem("entry", JSON.stringify(entry));
-    // Save allEntries back to local storage
-    existingEntries.push(entry);
-    localStorage.setItem("allEntries", JSON.stringify(existingEntries));
-};
+//set as global variable;
+let cityName = $("#cityText").val();
 
-$(document).ready(function ()
-$("#saveCityBtn").click(function(){
-    addCity;
+//create function to save city entered to local storage
+
+$(document).ready(function(){
+    
+     //get the city name from the element on button click
+     $("#saveCityBtn").click(function(){
+   //set city name inside click function too, why?
+   let cityName = $("#cityText").val();
+
+    //now set the item in local storage
+    localStorage.setItem('city', JSON.stringify(cityName));
+
+    //alert the value to make sure we got it right
+    alert(localStorage.getItem('city'));
+    console.log(cityName);
+
+    });  
+    
 });
-
+//why is this undefined out here?
+console.log(cityName);
 console.log(localStorage);
